bf01307013bdcd3e8fe44e7ef3b8736d092e392f
louisa :3 <louisakavindu@gmail.com>
2025-09-05 22:23:50 +0300

    trying to fix mobile?

--- components/launch/Button.tsx
+++ components/launch/Button.tsx
@@ -3,7 +3,7 @@ import { AnchorHTMLAttributes } from "react";
 export default function Button({ children, className, href }: AnchorHTMLAttributes<HTMLAnchorElement>) {
   return (
     <a href={href} className={`block bg-[#222] text-white p-1 ${className}`}>
-      <span className="border border-[#DBC491]/50 py-1 px-3 block font-serif uppercase">{children}</span>
+      <span className="border border-[#DBC491]/50 py-2 px-4 md:py-1 md:px-3 block font-serif uppercase text-sm md:text-base">{children}</span>
     </a>
   )
 }
